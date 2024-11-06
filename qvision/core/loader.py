1c4c2f2ac38e8cf868a43d30bf65f14afff2e810
0xD4rky <ishaan.kumar281@gmail.com>
2024-11-06 23:42:22 +0530

    model loading

--- qvision/core/loader.py
+++ qvision/core/loader.py
@@ -3,3 +3,11 @@ import torchvision.models as models
 import tensorflow as tf
 import onnx
 import onnxruntime as ort
+
+class ModelLoader:
+
+    def __init__(self):
+        pass
+
+    def load_pytorch_model(self, model_name = 'resnet18', pretrained = true, model_path = None):
+        
