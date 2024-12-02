1a532043049771489d3f8b937deee3f7b85df2e4
0xD4rky <ishaan.kumar281@gmail.com>
2024-12-02 22:53:39 +0530

    yolo->coco

--- qvision/core/loader.py
+++ qvision/core/loader.py
@@ -31,3 +31,15 @@ class Load(Dataset):
                 for line in f.readlines():
                     parts = line.strip().split()
                     class_id = int(parts[0])
+                    x_centre = float(parts[1])
+                    y_centre = float(parts[2])
+                    w = float(parts[3])
+                    h = float(parts[4])
+                    # converting YOLO format bb -> COCO format bb
+                    x1 = (x_centre - w/2) * width
+                    y1 = (y_centre - h/2) * height
+                    x2 = (x_centre + w/2) * width
+                    y2 = (y_centre + h/2) * height
+                    boxes.append([x1,y1,x2,y2])
+                    labels.append([class_id])
+        
