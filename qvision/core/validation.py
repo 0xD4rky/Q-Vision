d0df8093c6e27b70d4b70b75268b46b1ed9eae23
0xD4rky <ishaan.kumar281@gmail.com>
2025-01-25 23:30:15 +0530

    parsing cli

--- qvision/core/validation.py
+++ qvision/core/validation.py
@@ -1,3 +1,11 @@
 from ultralytics import YOLO
 import argparse
 import pathlib
+
+if __name__ == '__main__':
+
+    parser = argparse.ArgumentParser(description='validation model')
+    parser.add_argument('--model', type=str, default='model/yolov8n.engine', required=True, help='Path to the .pt')
+    parser.add_argument('--q', type=str, default='fp16', required=False, help='[fp16, int8]')
+    parser.add_argument('--data', type=str, default='coco.yaml', required=True, help='Dataset')
+    args = parser.parse_args()
