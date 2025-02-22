63cb901aaa63a7206249f8a5f4fc4b4512841675
0xD4rky <ishaan.kumar281@gmail.com>
2025-02-22 23:50:42 +0530

    pipelines added

--- quantization/qat.py
+++ quantization/qat.py
@@ -123,3 +123,26 @@ def get_model_size(model):
     os.remove('temp.p')
     return size
 
+
+transform_train = transforms.Compose([
+    transforms.RandomCrop(32, padding=4),
+    transforms.RandomHorizontalFlip(),
+    transforms.ToTensor(),
+    transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
+])
+
+transform_test = transforms.Compose([
+    transforms.ToTensor(),
+    transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
+])
+
+trainset = torchvision.datasets.CIFAR10(root='./data', train=True, download=True, transform=transform_train)
+testset = torchvision.datasets.CIFAR10(root='./data', train=False, download=True, transform=transform_test)
+
+train_loader = DataLoader(trainset, batch_size=128, shuffle=True, num_workers=2)
+test_loader = DataLoader(testset, batch_size=128, shuffle=False, num_workers=2)
+
+device = torch.device("mps" if torch.backends.mps.is_available() else "cpu")
+
+model = ResNet(Block, [2, 2, 2, 2])
+model = model.to(device)
